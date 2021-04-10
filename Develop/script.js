7cd01560c89361ff16fc5a3f9bf74deb89ea6fe2
chdonovan <chrisdonovan311@gmail.com>
2021-04-09 19:37:57 -0500

    variables added to JS sheet

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,34 @@
 // Assignment code here
 
 
+
+// variables for password
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowerCase;
+// Password variable values
+
+character =  "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [1,2,3,4,5,6,7,8,9];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+space = [];
+var choices;
+
+//uppercase conversion
+var toUpper = function (x){
+  return x.toUpperCase();
+};
+
+alpha2 = alpha.map(toUpper);
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function(){
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
